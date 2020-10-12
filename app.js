82203300abdd0bea4d98eeba6aca92314362ca11
cboswel1 <christianboswell86@gmail.com>
2020-10-11 18:07:49 -0600

    inital inquirer prompts working

--- app.js
+++ app.js
@@ -1,16 +1,92 @@
+//dependencies
 const mysql = require("mysql"); 
-const inquirer = require("requirer"); 
+const inquirer = require("inquirer"); 
+require("console.table");
 
+//sql
 const connection = mysql.createConnection({
     host: "localhost", 
     port: 3306, 
     user: "root", 
     password: "Tjul869511!", 
-    database: "employeesDB", 
+    database: "employeesDB"
 }); 
 
-connection.connect(function(err) {
+//sql connect to db
+connection.connect(function (err) {
     if (err) throw err; 
-    console.log("connected!", connection.threadId); 
+    
 
+    //run function 
+    startPrompt();
 });
+
+//initial prompt to ask questions of what user would like to do
+
+async function startPrompt() {
+      console.log("working");
+      try {
+        const choice = await inquirer.prompt([
+          {
+            type: "list",
+            name: "ownerChoice",
+            message: "What would you like to do?",
+            choices: [
+              "View Employees", 
+              "View Employees by Department", 
+              "Add Employee", 
+              "Remove Employees", 
+              "Update Employee Role", 
+              "Add Role", 
+              "Quit"
+            ]
+          }
+        ])
+    
+        //conditional switch statement for options
+    
+        .then(function ({ ownerChoice }) {
+          switch (ownerChoice) {
+            case "View Employees":
+              viewEmployee();
+              break;
+            case "View Employees by Department":
+              viewEmployeesByDept(); 
+              break; 
+            case "Add Employee": 
+              addEmployee(); 
+              break; 
+            case "Remove Employee": 
+              removeEmployee(); 
+              break; 
+            case "Update Employee Role": 
+              updateEmpRole();
+              break; 
+            case "Add Role":
+              addRole(); 
+              break; 
+            case "Quit": 
+              connection.end();
+              break;
+          }
+        });
+      } catch (err) {
+        console.log(err);
+    }
+    }
+   
+
+// function View Employees 
+
+// function View Employees by Department 
+
+// function Add Employee 
+
+// function Remove Employees 
+
+// function Update Employees Role 
+
+// function Add Role 
+
+// function Quit 
+
