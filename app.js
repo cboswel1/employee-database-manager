aedd7fd2ea8939dddee10b9934740adcf40831fe
cboswel1 <christianboswell86@gmail.com>
2020-10-12 18:33:58 -0600

    created employees seed

--- app.js
+++ app.js
@@ -1,104 +1,101 @@
 //dependencies
-const mysql = require("mysql"); 
-const inquirer = require("inquirer"); 
+const mysql = require("mysql");
+const inquirer = require("inquirer");
 require("console.table");
 
 //sql
 const connection = mysql.createConnection({
-    host: "localhost", 
-    port: 3306, 
-    user: "root", 
-    password: "Tjul869511!", 
-    database: "employeesDB"
-}); 
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Tjul869511!",
+  database: "employeesDB",
+});
 
 //sql connect to db
 connection.connect(function (err) {
-    if (err) throw err; 
-    
+  if (err) throw err;
 
-    //run function 
-    startPrompt();
+  //run function
+  startPrompt();
 });
 
 //initial prompt to ask questions of what user would like to do
 
 function startPrompt() {
-      console.log("working");
-      inquirer.
-      prompt(
-          {
-            type: "list",
-            name: "ownerChoice",
-            message: "What would you like to do?",
-            choices: [
-              "View Employees", 
-              "View Employees by Department", 
-              "Add Employee", 
-              "Remove Employees", 
-              "Update Employee Role", 
-              "Add Role", 
-              "Quit"
-            ]
-          })
-    
-        //conditional switch statement for options
-    
-        .then(function ({ ownerChoice }) {
-          switch (ownerChoice) {
-            case "View Employees":
-              viewEmployee();
-              break;
-            case "View Employees by Department":
-              viewEmployeesByDept(); 
-              break; 
-            case "Add Employee": 
-              addEmployee(); 
-              break; 
-            case "Remove Employee": 
-              removeEmployee(); 
-              break; 
-            case "Update Employee Role": 
-              updateEmpRole();
-              break; 
-            case "Add Role":
-              addRole(); 
-              break; 
-            case "Quit": 
-              connection.end();
-              break;
-          }
-        });
-    }
-   
+  console.log("working");
+  inquirer
+    .prompt({
+      type: "list",
+      name: "ownerChoice",
+      message: "What would you like to do?",
+      choices: [
+        "View Employees",
+        "View Employees by Department",
+        "Add Employee",
+        "Remove Employees",
+        "Update Employee Role",
+        "Add Role",
+        "Quit",
+      ],
+    })
 
-// function View Employees 
+    //conditional switch statement for options
+
+    .then(function ({ ownerChoice }) {
+      switch (ownerChoice) {
+        case "View Employees":
+          viewEmployee();
+          break;
+        case "View Employees by Department":
+          viewEmployeesByDept();
+          break;
+        case "Add Employee":
+          addEmployee();
+          break;
+        case "Remove Employee":
+          removeEmployee();
+          break;
+        case "Update Employee Role":
+          updateEmpRole();
+          break;
+        case "Add Role":
+          addRole();
+          break;
+        case "Quit":
+          connection.end();
+          break;
+      }
+    });
+}
+
+// function View Employees
 function viewEmployee() {
-    console.log("VE Working");
-    
-        var query = hjkl
-    
+  console.log("VE Working");
+
+  // https://dev.mysql.com/doc/refman/8.0/en/outer-join-simplification.html
+
+  var query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary, CONCAT(manager.first_name, ' ', manager.last_name)`;
 }
 
-// function View Employees by Department 
+// function View Employees by Department
 function viewEmployeesByDept() {
-    console.log("VEBD Working");
+  console.log("VEBD Working");
 }
-// function Add Employee 
+// function Add Employee
 function addEmployee() {
-    console.log("Add Employee");
+  console.log("Add Employee");
 }
-// function Remove Employees 
+// function Remove Employees
 function removeEmployee() {
-    console.log("Remove Employee");
+  console.log("Remove Employee");
 }
-// function Update Employees Role 
+// function Update Employees Role
 function updateEmpRole() {
-    console.log("Update Employee Role");
+  console.log("Update Employee Role");
 }
-// function Add Role 
+// function Add Role
 function addRole() {
-    console.log("Add Role");
+  console.log("Add Role");
 }
-// function Quit 
-
+// function Quit
--- sqlfiles/employeesDBSeeds.sql
+++ sqlfiles/employeesDBSeeds.sql
@@ -9,3 +9,31 @@ INSERT INTO department (name)
 VALUES ("Finance"); 
 INSERT INTO department (name) 
 VALUES ("Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1); 
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lead Engineer", 150000, 3); 
+INSERT INTO role (title, salary, department_id)
+VALUES ("Software Engineer", 120000, 4); 
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 125000, 6); 
+INSERT INTO role (title, salary, department_id)
+VALUES ("Legal Team Lead", 250000, 7); 
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 1, 3); 
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Mike", "Chan", 2, 1); 
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Ashley", "Rodriguez", 3, null); 
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Kevin", "Tupik", 4, 3); 
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Malia", "Brown", 6, null); 
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Sarah", "Lourd", 7, null); 
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Tom", "Allen", 8, 7); 
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Chris", "Eckenrode", 9, 7); 
